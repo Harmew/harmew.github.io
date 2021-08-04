ae5f7ceb83dfb4140264ec265fb280228d791e68
Harmew <dann.ew@outlook.com>
2021-08-03 22:53:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ function onClickMenu() {
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(' a[href^="#"]')
+  function scrollToSection(element) {
+    element.preventDefault()
+    const href = element.currentTarget.getAttribute('href');
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
+} initScrollSuave();
